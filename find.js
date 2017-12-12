8912cbd9959e70bf391f2382dcc3bfbb8f657380
vivienlee00 <vivien.lee.601@gmail.com>
2017-12-12 12:16:09 -0500

    started

--- find.js
+++ find.js
@@ -13,12 +13,16 @@ console.log( "box width: " + boxWidth );
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
-  /* YOUR IMPLEMENTATION */
+    /* YOUR IMPLEMENTATION */
+    var xdist = x1 - x0;
+    var ydist = y1 - y0;
+    return (sqrt(xdist*xdist + ydist*ydist));
 };
 
 
 var findIt = function(e) {
-  /* YOUR IMPLEMENTATION */
+    /* YOUR IMPLEMENTATION */
+    dist = distance(e.X,e.Y,targetX,targetY);
 };
 
 /*
